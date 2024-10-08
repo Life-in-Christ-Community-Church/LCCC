ebdbf5113d8cfc7e771e5ab8d783ae37f804684d
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-08 17:28:52 +0200

    added swiper.js, continuing with the banner

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,35 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // cssMode: true,
+    effect: "fade",
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    keyboard: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });

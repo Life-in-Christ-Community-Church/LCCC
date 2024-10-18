9747ffeeafae94bf064a6c8f1748bf074d6b9c03
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-18 19:43:22 +0200

    disabled barbajs to check if it's the problem

--- src/assets/js/barba.js
+++ src/assets/js/barba.js
@@ -1,53 +1,53 @@
-'use strict';
+// 'use strict';
 
-import barba from '@barba/core';
-import gsap from 'gsap';
-import { animationEnter, animationLeave } from './animations';
+// import barba from '@barba/core';
+// import gsap from 'gsap';
+// import { animationEnter, animationLeave } from './animations';
 
-import { initSwipers } from './swiper';
-import { initResponsiveMenu } from './responsive';
-import { initAboutContainer, initAboutScrollAnchor, initAboutUrlCheck } from './about-container';
+// import { initSwipers } from './swiper';
+// import { initResponsiveMenu } from './responsive';
+// import { initAboutContainer, initAboutScrollAnchor, initAboutUrlCheck } from './about-container';
 
-barba.hooks.once(() => {
-    console.log("once");
-    initSwipers();
-    initResponsiveMenu();
-    initAboutScrollAnchor();
-    initAboutContainer();
-    initAboutUrlCheck();
-})
+// barba.hooks.once(() => {
+//     console.log("once");
+//     initSwipers();
+//     initResponsiveMenu();
+//     initAboutScrollAnchor();
+//     initAboutContainer();
+//     initAboutUrlCheck();
+// })
 
-barba.hooks.after(() => {
-    console.log("after");
-    initSwipers();
-    initResponsiveMenu();
-    initAboutScrollAnchor();
-    initAboutContainer();
-    initAboutUrlCheck();
-})
+// barba.hooks.after(() => {
+//     console.log("after");
+//     initSwipers();
+//     initResponsiveMenu();
+//     initAboutScrollAnchor();
+//     initAboutContainer();
+//     initAboutUrlCheck();
+// })
 
-barba.init({
-    preventRunning: true,
-    schema: {
-        prefix: 'data-router',
-    },
-    transitions: [
-        {
-            once({next}){
-                animationEnter(next.container);
-                console.log("ONCE TRANSITION")
-                // initSwipers();
-                initResponsiveMenu();
-            },
-            leave: ({current}) => animationLeave(current.container),
-            enter({next}){
-                console.log("ENTER TRANSITION")
-                animationEnter(next.container);
-                window.scrollTo(0, 0)
-            },
-        }
-    ],
-    views: [
+// barba.init({
+//     preventRunning: true,
+//     schema: {
+//         prefix: 'data-router',
+//     },
+//     transitions: [
+//         {
+//             once({next}){
+//                 animationEnter(next.container);
+//                 console.log("ONCE TRANSITION")
+//                 // initSwipers();
+//                 initResponsiveMenu();
+//             },
+//             leave: ({current}) => animationLeave(current.container),
+//             enter({next}){
+//                 console.log("ENTER TRANSITION")
+//                 animationEnter(next.container);
+//                 window.scrollTo(0, 0)
+//             },
+//         }
+//     ],
+//     views: [
 
-    ]
-})
+//     ]
+// })

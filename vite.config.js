18ba6a7955c332e4759dc7d6cfb6a5176393befe
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-18 20:48:29 +0200

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
 import restart from 'vite-plugin-restart'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../public', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+export default defineConfig({
+    root: 'src/', // Carpeta donde se encuentran tus archivos fuente
+    publicDir: '../public', // Carpeta donde se encuentran los archivos estáticos (como _redirects)
+    server: {
+        host: true, // Abre al servidor en la red local y muestra la URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Abre si no es un CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+    build: {
+        outDir: '../dist', // Carpeta donde se generan los archivos de salida
+        emptyOutDir: true, // Vacía la carpeta antes de generar nuevos archivos
+        sourcemap: true, // Agrega sourcemap
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'), // Página principal
+                about: resolve(__dirname, 'src/about/index.html'), // Página de About
+                sermons: resolve(__dirname, 'src/sermons/index.html'), // Página de Sermones
+            }
+        }
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    plugins: [
+        restart({ restart: ['../static/**',] }) // Reinicia el servidor en cambios de archivos estáticos
     ],
-}
+});

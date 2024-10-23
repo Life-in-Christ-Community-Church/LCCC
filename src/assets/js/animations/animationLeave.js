f1aa1315223901a7e54006bee7950e61a44ffbb5
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-23 10:09:32 +0200

    Commented preload pictures due to warning

--- src/assets/js/animations/animationLeave.js
+++ src/assets/js/animations/animationLeave.js
@@ -3,7 +3,7 @@ import gsap from "gsap";
 const animationLeave = (container) => {
     const main = container.querySelector("main");
     const footer = container.querySelector("footer");
-    console.log(footer);
+
     const sectionBanner = container.querySelector("#sectionBanner")
     const span = container.querySelectorAll("a.is-active span")
     

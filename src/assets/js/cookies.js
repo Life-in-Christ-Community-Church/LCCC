976aaaffba22d3d83cec54a13dbed94d39770605
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-31 09:20:33 +0100

    Updated logic when cookie consent is together with privacy-policy button

--- src/assets/js/cookies.js
+++ src/assets/js/cookies.js
@@ -14,6 +14,11 @@ export const initCookies = () => {
     const mapsFooter = document.querySelector("#divMap");
     const mapsAbout = document.querySelector("#mapsAbout");
 
+    // Privacy-Policy Page
+    const button = document.querySelector(".checkboxPrivacy");
+    const cookieCheck = document.querySelector("#cookieCheck");
+    const cookieCheck2 = document.querySelector("#cookieCheck2");
+
     // Cookie Background
     function createCookieBackground(rounded, index) {
         const cookieBackground = document.createElement("div");
@@ -96,6 +101,12 @@ export const initCookies = () => {
         mapsFooter.appendChild(createMapsIframe("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d446.29915019311886!2d4.542581021718042!3d52.03976054622692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5ce8b51db2aab%3A0x68f7bce1aeb98992!2sKruisweg%2014%2C%202665%20HC%20Bleiswijk%2C%20Pa%C3%ADses%20Bajos!5e0!3m2!1ses!2ses!4v1728495288642!5m2!1ses!2ses",
             "Google Maps", 0));
 
+        // Privacy-Page logic
+        button.checked = true;
+        cookieCheck.innerHTML = "activated";
+        cookieCheck.style.color = "green"
+        cookieCheck2.innerHTML = "disable"
+
     });
 
     // Cookie Reject Button
@@ -135,10 +146,10 @@ export const initCookies = () => {
             "Google Maps", 0));
 
     } else {
-        
+
         if (sessionStorage.getItem("COOKIES") !== "REJECTED")
-        document.querySelector("#cookieDiv").classList.remove("hidden");
-        setTimeout(()=> {
+            document.querySelector("#cookieDiv").classList.remove("hidden");
+        setTimeout(() => {
             document.querySelector("#cookieDiv").style.transform = "translateY(0)";
         })
 
